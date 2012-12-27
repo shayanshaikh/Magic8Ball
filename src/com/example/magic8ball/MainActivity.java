e1d2c9ee34504fb687b4737299477ef357eaac22
Shayan Shaikh <shayanshaikh2@gmail.com>
2012-12-27 09:30:58 -0800

    Initial Commit

---
+++ src/com/example/magic8ball/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.magic8ball;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private MagicBall mMagicBall = new MagicBall();
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		final TextView answerLabel = (TextView) findViewById(R.id.textView1);
+		Button getAnswerButton = (Button) findViewById(R.id.button1);
+		
+		getAnswerButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View arg0) {
+				String answer = mMagicBall.getAnAnswer();
+				
+				answerLabel.setText(answer);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
