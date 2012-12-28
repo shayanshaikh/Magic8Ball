b36b9b1d5a26feb2133969fff7e97e144fc3f114
Shayan Shaikh <shayanshaikh2@gmail.com>
2012-12-28 15:45:14 -0800

    Added sound and text animation

--- src/com/example/magic8ball/MainActivity.java
+++ src/com/example/magic8ball/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.magic8ball;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,35 +15,61 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	private MagicBall mMagicBall = new MagicBall();
+	private TextView mAnswerLabel;
+	private Button mGetAnswerButton;
+	private ImageView mMagicBallImage;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		final TextView answerLabel = (TextView) findViewById(R.id.textView1);
-		Button getAnswerButton = (Button) findViewById(R.id.button1);
+		//Assign the views from the layout file
+		mAnswerLabel = (TextView) findViewById(R.id.textView1);
+		mGetAnswerButton = (Button) findViewById(R.id.button1);
+		mMagicBallImage = (ImageView) findViewById(R.id.imageView1);
 		
-		getAnswerButton.setOnClickListener(new View.OnClickListener() {
+		mGetAnswerButton.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View arg0) {
 				String answer = mMagicBall.getAnAnswer();
 				
-				answerLabel.setText(answer);
+				mAnswerLabel.setText(answer);
 				
 				animateMagic8Ball();
+				animateAnswer();
+				playSound();
 			}
 		});
 	}
 
 	private void animateMagic8Ball() {
-		ImageView magicBallImage = (ImageView) findViewById(R.id.imageView1);
-		magicBallImage.setImageResource(R.drawable.ball_animation);
-		AnimationDrawable ballAnimation = (AnimationDrawable) magicBallImage.getDrawable();
+		mMagicBallImage.setImageResource(R.drawable.ball_animation);
+		AnimationDrawable ballAnimation = (AnimationDrawable) mMagicBallImage.getDrawable();
 		if (ballAnimation.isRunning()) {
 		ballAnimation.stop();
 		}
 		ballAnimation.start();
+	}
+	
+	
+	private void animateAnswer() {
+		AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1);
+		fadeInAnimation.setDuration(1500);
+		fadeInAnimation.setFillAfter(true);
+		
+		mAnswerLabel.setAnimation(fadeInAnimation);	
+		}
+	
+	private void playSound() {
+		MediaPlayer player = MediaPlayer.create(this, R.raw.crystal_ball);
+		player.start();
+		player.setOnCompletionListener(new OnCompletionListener() {
+			
+			public void onCompletion(MediaPlayer mp) {
+			mp.release();
+			}
+		});
 	}
 	
 	@Override
