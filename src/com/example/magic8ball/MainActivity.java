8e924fab49e3c340d7de10d727fb1646aff2ed2b
Shayan Shaikh <shayanshaikh2@gmail.com>
2012-12-30 15:23:10 -0800

    Revert "Revert "added debugging""
    This reverts commit a16f54f5d121e07ba941f92841b8eb8657479c95.

--- src/com/example/magic8ball/MainActivity.java
+++ src/com/example/magic8ball/MainActivity.java
@@ -7,6 +7,7 @@ import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
@@ -15,6 +16,8 @@ import android.widget.TextView;
 import com.example.magic8ball.ShakeDetector.OnShakeListener;
 
 public class MainActivity extends Activity {
+	
+	public static final String TAG = MainActivity.class.getSimpleName();
 
 	private MagicBall mMagicBall = new MagicBall();
 	private TextView mAnswerLabel;
@@ -42,6 +45,9 @@ public class MainActivity extends Activity {
 			}
 		});
 		
+		//Toast.makeText(this, "Yay! Our Activity was created!", Toast.LENGTH_LONG).show();
+		
+		Log.d(TAG, "We're logging from the onCreate() method!");
 	}
 	@Override
 	public void onResume() {
